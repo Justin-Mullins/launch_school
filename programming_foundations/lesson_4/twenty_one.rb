baf32d2d7bdd8c317d59db109166f48bf955de14
Justin Mullins <justin.mullins009@gmail.com>
2016-01-23 19:28:21 -0600

    Added more to twenty_one.rb

--- programming_foundations/lesson_4/twenty_one.rb
+++ programming_foundations/lesson_4/twenty_one.rb
@@ -4,6 +4,26 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+def display_hands(dealer_hand, player_hand)
+  system 'clear'
+
+  prompt 'Twenty One'
+  prompt '-----------'
+  prompt "Dealer has: " + joinor(dealer_hand, ', ', 'and')
+  prompt "Player has: " + joinor(player_hand, ', ', 'and')
+end
+
+def joinor(array, separator=', ', final_separator='or')
+  string = ''
+  if array.length <= 1
+    return array.join
+  elsif array.length > 2
+    string = array.first(array.length - 2).join("#{separator}") +
+             "#{separator}"
+  end  
+  string += array.last(2).join(" #{final_separator} ")
+end
+
 # create deck and shuffle 
 def initialize_deck
   deck = []
@@ -18,28 +38,90 @@ def deal_cards(deck)
   cards = deck.pop(2)
 end
 
-system 'clear'
-prompt 'Welcome to Twenty One'
-prompt '----------------------'
+def player_turn(deck, player_hand)
+  prompt "Do you want to Hit or Stay?('h' or 's')"
+  choice = gets.chomp
 
-# 1. Initialize deck
-deck = initialize_deck
+  # hit
+  if choice.downcase.start_with?('h')
+    hit(deck, player_hand)
+  # stay
+  elsif choice.downcase.start_with?('s')
+    's'
+  else
+    prompt "That's not a valid choice."
+    player_turn(deck, player_hand)
+  end
+end
 
-# 2. Deal cards to player and dealer
-dealer_hand = deal_cards(deck)
-prompt "Dealer has: #{dealer_hand[0]} and #{dealer_hand[1]}"
+def hit(deck, player_hand)
+  player_hand << deck.pop
+end
 
-player_hand = deal_cards(deck)
-prompt "Player has: #{player_hand[0]} and #{player_hand[1]}"
+def bust(hand)
+  score = get_score(hand)
 
-# 3. player turn: hit or stay
-#  - repeat unil bust or stay
+  if score > 21
+    return true
+  end
+  false
+end
 
-# 4.If player bust, dealer wins.
+def get_score(hand)
+  score = 0
+  hand.each do |card|
+    if card == 'jack' || card == 'queen' || card == 'king'
+      score += 10
+    elsif card == 'ace'
+      if score > 21
+        score += 1
+      else
+        score += 11
+      end  
+    else
+      score += card.to_i
+    end
+  end
+  score
+end
 
-# 5. Dealer turn: hit or stay
-# - repeat until total >= 17
+loop do 
 
-# 6. If dealer bust, player wins.
+  loop do 
 
-# 7. Compare cards and declair winner.
+    # 1. Initialize deck
+    deck = initialize_deck
+
+    # 2. Deal cards to player and dealer
+    dealer_hand = deal_cards(deck)
+    player_hand = deal_cards(deck)
+
+    display_hands(dealer_hand, player_hand)
+
+    # 3. player turn: hit or stay
+    #  - repeat until bust or stay
+    stay = false
+    loop do
+      stay = player_turn(deck, player_hand)
+      break if stay == 's' || bust(player_hand)
+      display_hands(dealer_hand, player_hand)
+    end
+
+    # 4.If player bust, dealer wins.
+    if bust(player_hand)
+      display_hands(dealer_hand, player_hand)
+      prompt "Your total is #{get_score(player_hand)}. You bust!"
+      break
+    end
+
+    # 5. Dealer turn: hit or stay
+    # - repeat until total >= 17
+
+    # 6. If dealer bust, player wins.
+
+    # 7. Compare cards and declair winner.
+    prompt "#{who_won?(player_hand, dealer_hand)} won!"
+  end
+  prompt 'Do you want to play again?'
+end
+
