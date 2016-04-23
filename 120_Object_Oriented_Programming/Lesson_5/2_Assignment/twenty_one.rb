1e99d091ceee0e227f027e21c8ac8ee717aabbce
Justin Mullins <justin.mullins009@gmail.com>
2016-04-23 18:48:19 -0500

    Completed revising twenty_one.rb, and finished Lesson

--- 120_Object_Oriented_Programming/Lesson_5/2_Assignment/twenty_one.rb
+++ 120_Object_Oriented_Programming/Lesson_5/2_Assignment/twenty_one.rb
@@ -1,5 +1,4 @@
-# twenty-one.rb
-require 'pry'
+# twenty_one.rb
 
 module Hand
   def display_hand
@@ -12,15 +11,14 @@ module Hand
   end
 
   def busted?
-    return true if total_hand_value > 21
-    false
+    total_hand_value > 21
   end
 
   def total_hand_value
     total = 0
     num_of_aces = hand_values.count('Ace')
 
-    hand_values.each do |card| # go though each card in hand
+    hand_values.each do |card|
       if card == 'Jack' || card == 'Queen' || card == 'King'
         total += 10
       elsif card == 'Ace'
@@ -38,34 +36,41 @@ module Hand
   end
 
   def add_card(new_card)
-    cards << new_card
+    hand << new_card
   end
 end
 
 class Participant
   include Hand
+  attr_accessor :name, :hand
 
-  attr_accessor :name, :hand, :hand_values
   def initialize
     @hand = []
-    @hand_values = []
     set_name
   end
 
-  def set_hand_values
+  def hand_values
+    hand_values = []
     hand.each { |card| hand_values << card.value }
+    hand_values
+  end
+
+  def reset
+    @hand = []
   end
 end
 
 class Player < Participant
   def set_name
     name = ''
+
     loop do
       puts "What is your name?"
       name = gets.chomp
       break unless name.empty?
       puts "Sorry, you must enter a name."
     end
+
     self.name = name
   end
 end
@@ -73,6 +78,17 @@ end
 class Dealer < Participant
   def set_name
     self.name = 'Computer'
+  end
+
+  def display_hand
+    temp_hand = hand_values
+    temp_hand[0] = 'unknown'
+
+    puts "---- #{name}'s Hand ----"
+    temp_hand.each do |card|
+      puts "=> #{card}"
+    end
+    puts ""
   end
 end
 
@@ -84,28 +100,29 @@ class Deck
   def initialize
     @cards = []
     SUITS.each do |suit|
-      VALUES.each { |value| @cards << Card.new(value, suit) } # generate deck of Card objects
+      VALUES.each { |value| @cards << Card.new(value, suit) }
     end
 
     shuffle
   end
 
   def deal(num_of_cards)
-    @cards.pop(num_of_cards)
+    return cards.pop if num_of_cards == 1
+    cards.pop(num_of_cards)
   end
 
   def shuffle
-    @cards.shuffle!
+    cards.shuffle!
   end
 end
 
 class Card
   attr_reader :value, :suit
+
   def initialize(value, suit)
     @suit = suit
     @value = value
   end
-
 end
 
 class Game
@@ -118,32 +135,24 @@ class Game
   end
 
   def deal_initial_cards
-    player.hand = deck.deal(2) # deal each player 2 initial cards
+    player.hand = deck.deal(2)
     dealer.hand = deck.deal(2)
   end
 
-  def display_initial_cards
-    system "clear"
+  def display_welcome_message
     puts "Welcome to Twenty-One!"
-    puts ""
-    player.set_hand_values
-    player.display_hand
-    # add starting cards' values to hand_values
-    dealer.hand.each { |card| dealer.hand_values << card.value }
-    dealer.display_hand
-    puts ""
   end
 
   def display_cards
     system "clear"
-    puts "Twenty-One"
+    puts " === Twenty-One ==="
     puts ""
     player.display_hand
     dealer.display_hand
   end
 
   def hit_or_stay?
-    answer = nil 
+    answer = nil
     loop do
       puts "Hit or stay? (h/s)"
       answer = gets.chomp.downcase # get user input
@@ -154,45 +163,68 @@ class Game
   end
 
   def player_turn
-    while player.busted? == false
-      if hit_or_stay? == 'h' # if hit
-        player.hand.concat(deck.deal(1)) # deal 1 more card (object) to player (hand)
-        player.hand_values << player.hand.last.value # add the value of that card to hand_values
+    while !player.busted?
+      if hit_or_stay? == 'h'
+        player.add_card(deck.deal(1))
         display_cards
         puts "Player bust!" if player.busted?
       else
         break # if stay, break
-      end 
+      end
     end
   end
 
   def dealer_turn
-    while dealer.total_hand_value < 21 && dealer.total_hand_value <= player.total_hand_value && player.busted? == false
+    while dealer.total_hand_value < 21 && dealer.total_hand_value <= player.total_hand_value && !player.busted?
       sleep(0.5)
-      dealer.hand.concat(deck.deal(1)) # deal 1 more card (object) to dealer (hand)
-      dealer.hand_values << dealer.hand.last.value # add the value of that card to hand_values
+      dealer.add_card(deck.deal(1))
       display_cards
       puts "Dealer bust!" if dealer.busted?
     end
   end
 
   def show_result
+    puts "=> Dealer Total: #{dealer.total_hand_value}"
     if dealer.total_hand_value > player.total_hand_value && !dealer.busted?
       puts "You Lose!"
     elsif player.total_hand_value > dealer.total_hand_value && !player.busted?
       puts "You Win!"
-    elsif (dealer.busted? && player.busted?) || player.total_hand_value == dealer.total_hand_value
+    elsif (!dealer.busted? && !player.busted?) || player.total_hand_value == dealer.total_hand_value
       puts "It's a Tie."
     end
     puts ""
   end
 
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+      puts "Sorry, must be y or n."
+    end
+
+    answer == 'y'
+  end
+
+  def reset_game
+    sleep(1)
+    @deck = Deck.new
+    player.reset
+    dealer.reset
+  end
+
   def start
-    deal_initial_cards
-    display_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+    display_welcome_message
+    loop do
+      deal_initial_cards
+      display_cards
+      player_turn
+      dealer_turn
+      show_result
+      break unless play_again?
+      reset_game
+    end
   end
 end
 
