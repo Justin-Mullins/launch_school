5487b4909d83318adcdcd0b221dc70ea15ed1ae9
Justin Mullins <justin.mullins009@gmail.com>
2016-05-09 13:06:11 -0500

    Finished Todolist Test. Coursework for 130 - Ruby Foundations is complete.

---
+++ 130_Ruby_Foundations/Lesson_2/todolist.rb
@@ -0,0 +1,213 @@
+# todolist.rb
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  #-------Adding to list--------
+
+  def add(todo)
+    if todo.class == Todo
+      @todos << todo
+    else
+      raise TypeError.new("Can only add Todo objects")
+    end
+  end
+
+  def <<(todo)
+    if todo.class == Todo
+      @todos << todo
+    else
+      raise TypeError.new("Can only add Todo objects")
+    end
+  end
+  alias_method :add, :<<
+ 
+  #-------Interogating the list-------
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  #--------Retriving Item in the List------
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  #------Marking items done----------
+
+  def mark_done_at(index)
+    @todos.fetch(index).done!
+  end
+
+  def mark_undone_at(index)
+    @todos.fetch(index).undone!
+  end
+
+  def mark_all_done
+    @todos.each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    @todos.each { |todo| todo.undone! }
+  end
+
+  # marks the first Todo object that matches the argument as done.
+  def mark_done(string)
+    @todos.each do |todo|
+      todo.done! if (todo.title =~ /#{string}/)
+    end
+  end
+
+  #-------All Done-------
+
+  def done?
+    counter = 0
+    @todos.each { |todo| counter += 1 if todo.done? }
+    counter == @todos.length
+  end
+
+  def done!
+    @todos.each { |todo| todo.done! }
+  end
+
+  #-------Deleting from list-------
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    check = @todos.delete_at(index)
+    if check.nil?
+      raise IndexError.new("Outside of bounds of array.")
+    end
+  end
+
+  #------Iterating through list-----
+
+  def each
+    counter = 0
+    while counter < @todos.length do
+      yield(@todos[counter])
+      counter += 1
+    end
+    self
+  end
+
+  #------Selecting elements in list-----
+
+  def select
+    counter = 0
+    new_list = TodoList.new(title)
+
+    while counter < @todos.length do
+      if yield(@todos[counter])
+        new_list << @todos[counter]
+      end
+      counter += 1
+    end
+
+    new_list
+  end
+
+  #----Changes Todos all to done/undone----
+
+  def all_done
+    select{ |todo| todo.done? }
+  end
+
+  def all_not_done
+    select{ |todo| !todo.done? }
+  end
+
+  #-----Returns Todo that matches argument----
+
+  def find_by_title(string)
+    @todos.each do |todo|
+      return todo if (todo.title =~ /#{string}/)
+    end
+    nil
+  end
+
+
+  #--------Conversion methods-----------
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# add
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+
+puts "Searching for Todo: #{list.find_by_title("ilk")}"
+
+list.to_s
+list.mark_done('gym')
+puts ''
+
+list.to_s
+puts ''
+
+
+list.all_done.to_s
---
+++ 130_Ruby_Foundations/Lesson_2/todolist_test.rb
@@ -0,0 +1,157 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'todolist'
+
+class TodoListTest < MiniTest::Test
+
+  def setup
+    @todo1 = Todo.new("Buy milk")
+    @todo2 = Todo.new("Clean room")
+    @todo3 = Todo.new("Go to gym")
+    @todos = [@todo1, @todo2, @todo3]
+
+    @list = TodoList.new("Today's Todos")
+    @list.add(@todo1)
+    @list.add(@todo2)
+    @list.add(@todo3)
+  end
+
+  def test_to_a
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_size
+    assert_equal(@todos.size, @list.size)
+  end
+
+  def test_first
+    assert_equal(@todos.first, @list.first)
+  end
+
+  def test_last
+    assert_equal(@todos.last, @list.last)
+  end
+
+  def test_shift
+    todo = @list.shift
+    assert_equal(@todo1, todo)
+    assert_equal([@todo2, @todo3], @list.to_a)
+  end
+
+  def test_pop
+    todo = @list.pop
+    assert_equal(@todo3, todo)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
+
+  def test_done?
+    assert_equal(false, @list.done?)
+  end
+
+  def test_for_type_error
+    assert_raises(TypeError) do
+      @list << 'test'
+    end
+  end
+
+  def test_add_alias
+    new_todo = Todo.new("Walk the dog")
+    @list.add(new_todo)
+    todos = @todos << new_todo
+
+    assert_equal(todos, @list.to_a)
+  end
+
+  def test_item_at
+    assert_raises(IndexError) { @list.item_at(100) }
+    assert_equal(@todo1, @list.item_at(0))
+    assert_equal(@todo2, @list.item_at(1))
+  end
+
+  def test_mark_done_at
+    assert_raises(IndexError) do
+      @list.mark_done_at(10)
+    end
+
+    @list.mark_done_at(1)
+    assert_equal(false, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(false, @todo3.done?)
+  end
+
+  def test_mark_undone_at
+    assert_raises(IndexError) do
+      @list.mark_undone_at(100)
+    end
+    @todo1.done!
+    @todo2.done!
+    @todo3.done!
+
+    @list.mark_undone_at(1)
+    assert_equal(true, @todo1.done?)
+    assert_equal(false, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+  end
+
+  def test_done!
+    @list.done!
+    assert_equal(true, @todo1.done?)
+    assert_equal(true, @todo2.done?)
+    assert_equal(true, @todo3.done?)
+    assert_equal(true, @list.done?)
+  end
+
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(10) }
+    @list.remove_at(1)
+    assert_equal([@todo1, @todo3], @list.to_a)
+  end
+
+  def test_to_s
+    output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [ ] Clean room
+    [ ] Go to gym
+    OUTPUT
+    assert_equal(output, @list.to_s)
+
+    @list.mark_done_at(1)
+    output2 = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [X] Clean room
+    [ ] Go to gym
+    OUTPUT
+    assert_equal(output2, @list.to_s)
+
+    @list.done!
+    output3 = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [X] Buy milk
+    [X] Clean room
+    [X] Go to gym
+    OUTPUT
+    assert_equal(output3, @list.to_s)
+  end
+
+  def test_each
+    index = 0
+    list_copy = @list.each do |todo|
+      assert_equal(todo, @list.item_at(index))
+      index += 1
+    end
+    assert_equal(@list, list_copy)
+  end
+
+  def test_select
+    @todo1.done!
+    list = TodoList.new(@list.title)
+    list.add(@todo1)
+
+    assert_equal(list.title, @list.title)
+    assert_equal(list.to_s, @list.select{ |todo| todo.done? }.to_s)
+  end
+end
