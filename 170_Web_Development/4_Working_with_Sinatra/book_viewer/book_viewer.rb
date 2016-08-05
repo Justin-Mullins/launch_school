f095d06c12391fc70b10c8039e4da415cdf5545d
Justin Mullins <justin.mullins009@gmail.com>
2016-08-04 21:36:34 -0500

    Finished Todo Lists

--- 170_Web_Development/4_Working_with_Sinatra/book_viewer/book_viewer.rb
+++ 170_Web_Development/4_Working_with_Sinatra/book_viewer/book_viewer.rb
@@ -1,18 +1,44 @@
+# encoding: utf-8
+
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+
+  def in_paragraphs(text)
+    text.split("\n\n").map do |line|
+      "<p>#{line}</p>"
+    end.join
+  end
+
+end
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/search" do
+  # @results = chapter_matching(params[:query])
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
